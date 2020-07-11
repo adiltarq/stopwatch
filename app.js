a7617d428ae90cb1191e8a09da17ec69baed70a1
adiltarq <64355952+adiltarq@users.noreply.github.com>
2020-07-11 15:18:58 +0500

    functionality done

---
+++ app.js
@@ -0,0 +1,40 @@
+var minh = document.getElementById("min");
+var sech = document.getElementById("sec");
+var msech = document.getElementById("msec");
+
+var min = 0;
+var sec = 0;
+var msec = 0;
+var interval;
+function show(){
+    msec++
+    msech.innerHTML = msec
+    console.log(msec)
+    if(msec >= 100){
+        sec++
+        sech.innerHTML = sec
+        msec = 0;
+    }else if(sec >= 60){
+        min++
+        minh.innerHTML = min;
+        sec = 0;
+    }
+}
+
+function start(){
+    interval = setInterval(show,10)
+    document.getElementById("start").disabled = true;
+}
+function stop(){
+    clearInterval(interval)
+    document.getElementById("start").disabled = false;
+}
+function reset(){
+    min = 0;
+    minh.innerHTML = min;
+    sec = 0;
+    sech.innerHTML = sec;
+    msec = 0;
+    msech.innerHTML = msec;
+    stop()
+}
